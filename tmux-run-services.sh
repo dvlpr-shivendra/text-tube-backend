ac0a80ba544325da499c03bc22fd97e53d0f1e70
Shivendra Kumar <shivendratechster@gmail.com>
2025-11-04 21:20:59 +0530

    fix tmux setup

--- tmux-run-services.sh
+++ tmux-run-services.sh
@@ -1,5 +1,8 @@
-tmux new-session -d -s texttube 'cd auth-service && go run ./cmd/main.go'
-tmux split-window -h 'cd video-service && go run ./cmd/main.go'
-tmux split-window -v 'cd gateway && go run ./cmd/main.go'
+#!/bin/bash
+set -e
+
+tmux new-session -d -s texttube 'cd auth-service && go run ./cmd/main.go; exec bash'
+tmux split-window -h 'cd video-service && go run ./cmd/main.go; exec bash'
+tmux split-window -v 'cd gateway && go run ./cmd/main.go; exec bash'
 tmux select-pane -t 0
 tmux attach-session -t texttube
